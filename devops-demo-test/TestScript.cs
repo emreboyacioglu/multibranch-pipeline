dfbd1ba581ac43a6f277df656c754f65b7b26df7
YUNUS EMRE BOYACIOGLU <Emreb@tr.ibm.com>
2020-10-01 04:37:21 +0300

    xunit test

---
+++ devops-demo-test/TestScript.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace devops_demo_test
+{
+    public class TestScript
+    {
+        [Fact]
+        public void XUnitTest()
+        {
+            Assert.True(1 == 1);
+        }
+        [Fact]
+        public void XUnitTestOther()
+        {
+            Assert.True(true);
+        }
+    }
+}
